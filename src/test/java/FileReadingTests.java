0acc967698bf2b53a3baa8b694fd3a63a9ca1571
gsi-felix <felix@generalsoftwareinc.com>
2019-01-07 18:56:21 -0500

    feat: Added tests for checking STDOUT from sample data ('city', 'id') criteria

--- src/test/java/FileReadingTests.java
+++ src/test/java/FileReadingTests.java
@@ -6,8 +6,8 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FileReadingTests {
 
@@ -22,13 +22,29 @@ public class FileReadingTests {
     }
 
     @Test
-    public void stress() {
+    public void checkPeopleId() {
+        final String personId1 = "25384390A";
+        final String personId2 = "25384390-A";
+
+        final String[] args1 = new String[]{Objects.requireNonNull(getSample()).toString(), "ID", personId1};
+        final String[] args2 = new String[]{Objects.requireNonNull(getSample()).toString(), "ID", personId2};
+        List output1 = ProcessorFactory.from(args1).create();
+        List output2 = ProcessorFactory.from(args2).create();
+
+        assert !output1.isEmpty() && output1.get(0) instanceof PeopleTuple;
+        assert ((PeopleTuple) output1.get(0)).getId().equals(personId1);
+        assert ((PeopleTuple) output2.get(0)).getId().equals(personId2);
+
+    }
+
+    @Test
+    public void checkCity() {
         final String city = "BARCELONA";
-        final String[] args = new String[]{getSample().toString(), "CITY", city};
+        final String[] args = new String[]{Objects.requireNonNull(getSample()).toString(), "CITY", city};
         List output = ProcessorFactory.from(args).create();
 
         assert output.size() == 9;
 
-        assert output.stream().allMatch(person -> ((ArrayList<PeopleTuple>) person).get(0).getCity().equalsIgnoreCase(city));
+        assert output.stream().allMatch(person -> ((PeopleTuple) person).getCity().equalsIgnoreCase(city));
     }
 }
